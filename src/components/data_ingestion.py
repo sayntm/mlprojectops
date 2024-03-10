d2eb00aab9bead93e77438dd8745528e64f5d643
sayantams <sayantam.s@agribridge.in>
2024-03-10 21:07:30 +0530

    Data Transformation & Model Training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,12 @@ from src.exception import CustomException
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts', "train.csv")
@@ -42,4 +48,14 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    train_arr, test_arr = data_transformation.initiate_data_transformation(train_data, test_data)
+    
+    model_trainer = ModelTrainer()
+    print (model_trainer.initiate_model_trainer(train_arr, test_arr))
+     
+    
+    
+# python -m src.components.data_ingestion
